e3065f48cd6e27f80c2679bfca834219a3ab27a7
Mr. Wayne <70208852+a4arpon@users.noreply.github.com>
2023-05-17 20:12:41 +0000

    Fix: Server Issue Can not get fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-
+const { MongoClient, ServerApiVersion } = require('mongodb')
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -9,7 +9,6 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clustertest.wemsww6.mongodb.net/?retryWrites=true&w=majority`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,28 +22,21 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect()
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
     )
     // Server Code
-    app.get('/', (req, res) => {
-      res.send('Hello World')
+    const toysCollection = client.db('toyDb').collection('toys')
+    app.get('/', async (req, res) => {
+      const result = await toysCollection.find().toArray()
+      res.send(result)
     })
   } finally {
   }
 }
-run().catch(console.dir)
-
-app.post('/api/users', (req, res) => {
-  const { name, email } = req.body
-  // Save user to database or perform other operations
-  res.json({ message: 'User created successfully!' })
-})
-
+run()
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
